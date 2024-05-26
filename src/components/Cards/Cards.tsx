767b5ffcd88fbf7e3b10e604e22aa73d3b4a5284
Manas Ranjan Singh <manassingh900@gmail.com>
2024-05-26 19:04:12 +0530

    refactor_cards_component

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -5,7 +5,7 @@ interface Props {
   card: CardObj;
 }
 const Cards = ({ card }: Props) => {
-  return card.bestSeller ? (
+  return (
     <div className={styles.card}>
       <div className={styles.imgContainer}>
         <img src={`./${card.img}`} className={styles.img} />
@@ -24,28 +24,7 @@ const Cards = ({ card }: Props) => {
         <p className={styles.price}>
           <b>{card.price}</b>
         </p>
-        <div className={styles.bestSeller}>Bestseller</div>
-      </div>
-    </div>
-  ) : (
-    <div className={styles.card}>
-      <div className={styles.imgContainer}>
-        <img src={`./${card.img}`} className={styles.img} />
-      </div>
-      <div className={styles.cardDetailsContainer}>
-        <h4>{card.title}</h4>
-        <h5 className={styles.authorName}>
-          {card.author.length >= 34
-            ? `${card.author.substring(0, 37).substring(0, 37)}...`
-            : card.author}
-        </h5>
-        <p className={styles.rating}>
-          <b>{card.rating}</b> ⭐⭐⭐⭐{" "}
-          <span className={styles.totalRatings}>{card.totalRatings}</span>
-        </p>
-        <p className={styles.price}>
-          <b>{card.price}</b>
-        </p>
+        {card.bestSeller && <div className={styles.bestSeller}>Bestseller</div>}
       </div>
     </div>
   );
